d13634927780ad844521a761117ca5d194f9cdfe
rtaza <rtazaki@gmail.com>
2023-10-03 23:28:44 +0900

    change: ファイルオープンダイアログ実装

--- HelloAvalonia/ViewModels/Pages/_1PageViewModel.cs
+++ HelloAvalonia/ViewModels/Pages/_1PageViewModel.cs
@@ -1,8 +1,46 @@
-﻿namespace HelloAvalonia.ViewModels
+﻿using Avalonia.Controls;
+using Avalonia.Platform.Storage;
+using ReactiveUI;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Input;
+
+namespace HelloAvalonia.ViewModels
 {
     public class _1PageViewModel : ViewModelBase
     {
-        public string Title => "This is the first page.";
-        public string Message => "Hello.0123";
+        public ICommand FileOpenDialogCommand { get; }
+        private string _fileString = string.Empty;
+        public string FileString
+        {
+            get => _fileString;
+            set => this.RaiseAndSetIfChanged(ref _fileString, value);
+        }
+
+        public _1PageViewModel()
+        {
+            FileOpenDialogCommand = ReactiveCommand.Create<Window>(async (window) =>
+            {
+                var files = await window.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+                {
+                    Title = "ファイルを開く",
+                    FileTypeFilter = new List<FilePickerFileType>
+                    {
+                        new FilePickerFileType("オリジナル拡張子")
+                        {
+                            Patterns = new List<string>{ "*.cm*" },
+                        },
+                    },
+                    AllowMultiple = false,
+                }); ;
+
+                if (files.Count >= 1)
+                {
+                    await using var stream = await files[0].OpenReadAsync();
+                    using var streamReader = new StreamReader(stream);
+                    FileString = await streamReader.ReadToEndAsync();
+                }
+            });
+        }
     }
 }
